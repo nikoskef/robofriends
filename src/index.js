711f13333fb5708181c601ae1eb4d2fec048c9d9
nikoskef <nikoskef86@gmail.com>
2018-08-01 13:56:57 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
+
+ReactDOM.render(
+				<Provider store={store}>
+					<App />
+				</Provider>	
+				, document.getElementById('root'));
 registerServiceWorker();
